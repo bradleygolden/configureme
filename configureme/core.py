e5313f7fe334d97238d41989b41eefd8e30aa68d
f1x3d <oleksandr.manyk@gmail.com>
2018-10-11 01:03:45 +0300

    Allow default values for environment variables
    Also, remove the specific value when checking if the value exists

--- configureme/core.py
+++ configureme/core.py
@@ -4,26 +4,33 @@ import warnings
 
 from .helpers import dotenv_to_dict, str_to_py
 
-NOT_FOUND = "not found"
-
 
 class Config(dict):
     def __init__(self):
         self._aliases = dict()
+        self._defaults = dict()
         self.envs = list()
 
     def __getitem__(self, key):
         rv = None
         # environment variables always take precedence
         rv = self._env_value(key)
-        if not rv:
-            # no env variable value exists use the default if it exists
-            rv = self.get(key, NOT_FOUND)
-        if rv == NOT_FOUND:
-            raise ValueError(
-                f"Variable {key} value is not set as an environment variable and does not have a default value."
-            )
-        return rv
+        if rv is not None:
+            return rv
+        # no env variable value exists, use the config value if it exists
+        if key in self:
+            return self.get(key, None)
+        # no config value exists, use the default value if it exists
+        if key in self._defaults:
+            return self._defaults[key]
+        # no default value for the key, use the default value of its alias if it exists
+        if key in self._aliases:
+            key_alias = self._aliases[key]
+            if key_alias in self._defaults:
+                return self._defaults[key_alias]
+        raise ValueError(
+            f"Variable {key} value is not set as an environment variable or config value, and does not have a default value."
+        )
 
     def _env_value(self, key: str):
         rv = None
@@ -51,10 +58,13 @@ class Config(dict):
         for name, value in _store.items():
             self[name] = value
 
-    def from_envar(self, name: str, rename: str = None):
+    def from_envar(self, name: str, rename: str = None, default: str = None):
         """Set configuration from an environment variable."""
         if not os.getenv(name):
             warn_msg = f"Environment variable '{name}' not found."
             warnings.warn(warn_msg, RuntimeWarning)
-        elif rename:
+            return
+        if rename is not None:
             self._aliases[rename] = name
+        if default is not None:
+            self._defaults[name] = default
